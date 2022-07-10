b30f4cc763bc18ecd7ac723b11bc9fa8696c9cd6
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-07-10 17:14:37 +0530

    Snack Ladder Player Zero position

---
+++ Uc1_PlayerPosZero.java
@@ -0,0 +1,16 @@
+package Day_4;
+
+public class Uc1_PlayerPosZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+	 
+		int Position = 0;
+		System.out.println("Wecome To Snake Ladder Program");
+		int dice = (int) (Math.floor(Math.random()*10)%0+1);
+		
+		System.out.println("Player Position:" + dice);
+	}
+
+}
