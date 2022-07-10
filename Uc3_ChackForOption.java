2c740f11282b98b07401db8cc5bd52a36fa06c32
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-07-10 17:23:57 +0530

    Snack Ladder Check Option

---
+++ Uc3_ChackForOption.java
@@ -0,0 +1,41 @@
+package Day_4;
+
+public class Uc3_ChackForOption {
+
+	 public static  int diceRoll() {
+			int a = (int) ((Math.floor(Math.random()*10)%6)+1);
+			return a;
+			}
+		
+	public static  int genratePlayerOption() {
+			int b = (int) ((Math.floor(Math.random()*10)%3)+1);
+			return b;
+		}
+	 public static void main(String[] args) {
+		 Uc3_ChackForOption ls = new Uc3_ChackForOption();
+		 ls.diceRoll();
+		 ls.genratePlayerOption();
+		 System.out.println(diceRoll());
+		 System.out.println(genratePlayerOption());
+			
+	    int a = 0;
+		int b = 0;
+		
+		 int res;
+		switch (b) {
+	     case 1: res = a+b;
+	       System.out.println(res+"play snake");
+	       break;
+	     
+	     case 2: res = a-b;
+	       System.out.println(res+"play ladder");
+	       break;
+	     
+	     default: 
+	       System.out.println("position snake is zero");
+	     
+	     
+	     }		 
+	 }
+
+}
